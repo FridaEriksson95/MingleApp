98d91c67443ddf3b6d5156051681719f7de8edc0
Robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2024-12-30 13:44:04 +0100

    Created instances of Firebase auth and database

--- app/src/main/java/com/example/mingleapp/Repositories/FirebaseAuth.kt
+++ app/src/main/java/com/example/mingleapp/Repositories/FirebaseAuth.kt
@@ -1,4 +1,9 @@
 package com.example.mingleapp.Repositories
 
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
 class FirebaseAuth {
+
+    private val auth = Firebase.auth
 }
